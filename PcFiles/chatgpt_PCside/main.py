58308541805de988225756e7efa56becd4274b84
michelottialessandro <alessandro.michelotti1706@gmail.com>
2024-06-19 18:06:22 +0200

    last commit

--- PcFiles/chatgpt_PCside/main.py
+++ PcFiles/chatgpt_PCside/main.py
@@ -1,215 +1,143 @@
-import random 
-import argparse 
-import os 
-import wave 
-import pvporcupine 
-from pvrecorder import PvRecorder 
-import speech_recognition as sr 
-import websocket 
-from scipy.io import wavfile 
+import asyncio
+import websockets
 import json
-import serial
+import multiprocessing as mp
+from llama3_main import set_up_generator, get_response
+import current_time
+import transcribe
+import time
 import multiprocessing
-import subprocess
+from functools import partial
+import json
+import numpy as np
 
-arduino = serial.Serial(port='/dev/ttyUSB0', baudrate=115200, timeout=1) #LASCIA TIMEOUT A !
+def tronca_all_ultimo_punto(testo):
+    ultimo_punto = testo.rfind('.')
+    if ultimo_punto == -1:
+        return testo
+    troncato = testo[:ultimo_punto + 1]
+    troncato = troncato.replace('"', '')
+    troncato = troncato.replace("'", '')
+    return troncato
 
-r = sr.Recognizer() # Crea una istanza del recognizer
+def remove_quotes(string,is_response):
+    if(is_response==False):
+        string = string.replace('"', '')
+        string = string.replace("'", '')
+        string = string.replace("*", '')
+        string=string.lower()
 
-ACCESS_KEY="zvo3gDplLoZ2lxzG31HCdEdqoNemPth79NuTZqA5LpU6DteiwTlNXg=="
-KEYWORD_PATH="Hey-computer_en_raspberry-pi_v3_0_0.ppn"
-
-
-
-phrases=["i am listening, please go on","i am listening","i am all ears","Go ahead, I'm listening intently","I'm here to listen"]
-        
-
-def run_command(command):
-    subprocess.run(command, shell=True)
-
-def start_command(command):
-    global process
-    process = multiprocessing.Process(target=run_command, args=(command,))
-    process.start()
-
-def stop_command():
-    process.terminate()
-    process.join()
-
-
-def send_wav_file_and_get_response(websocket_url,data):
-    print("sending")
-    ws=websocket.create_connection(websocket_url)
-    ws.send(data,websocket.ABNF.OPCODE_BINARY)
-    response= ws.recv()
-    ws.close()
-    return response
-
-# WebSocket URL
-websocket_url = "ws://192.168.178.185:8765"
-
-
-def main():
-    parser = argparse.ArgumentParser()
-
-    parser.add_argument(
-        '--access_key',
-        help='AccessKey obtained from Picovoice Console (https://console.picovoice.ai/)')
-
-    parser.add_argument(
-        '--keywords',
-        nargs='+',
-        help='List of default keywords for detection. Available keywords: %s' % ', '.join(
-            '%s' % w for w in sorted(pvporcupine.KEYWORDS)),
-        choices=sorted(pvporcupine.KEYWORDS),
-        metavar='')
-
-    parser.add_argument(
-        '--keyword_paths',
-        nargs='+',
-        help="Absolute paths to keyword model files. If not set it will be populated from `--keywords` argument")
-
-    parser.add_argument(
-        '--library_path',
-        help='Absolute path to dynamic library. Default: using the library provided by `pvporcupine`')
-
-    parser.add_argument(
-        '--model_path',
-        help='Absolute path to the file containing model parameters. '
-             'Default: using the library provided by `pvporcupine`')
-
-    parser.add_argument(
-        '--sensitivities',
-        nargs='+',
-        help="Sensitivities for detecting keywords. Each value should be a number within [0, 1]. A higher "
-             "sensitivity results in fewer misses at the cost of increasing the false alarm rate. If not set 0.5 "
-             "will be used.",
-        type=float,
-        default=None)
-
-    parser.add_argument('--audio_device_index', help='Index of input audio device.', type=int, default=-1)
-
-    parser.add_argument('--output_path', help='Absolute path to recorded audio for debugging.', default=None)
-
-    parser.add_argument('--show_audio_devices', action='store_true')
-
-    args = parser.parse_args()
-
-    if args.show_audio_devices:
-        for i, device in enumerate(PvRecorder.get_available_devices()):
-            print('Device %d: %s' % (i, device))
-        return
-
-    if args.keyword_paths is None:
-        if args.keywords is None:
-            raise ValueError("Either `--keywords` or `--keyword_paths` must be set.")
-
-        keyword_paths = [pvporcupine.KEYWORD_PATHS[x] for x in args.keywords]
     else:
-        keyword_paths = args.keyword_paths
+        string = string.replace("*", '')
 
-    if args.sensitivities is None:
-        args.sensitivities = [0.5] * len(keyword_paths)
+    return string
 
-    if len(keyword_paths) != len(args.sensitivities):
-        raise ValueError('Number of keywords does not match the number of sensitivities.')
+def check_prompt(prompt):
+    prompt=prompt.lower()
+    if("what" in prompt and "time" in prompt and "is" in prompt):
+        return current_time.get_time("en")
+    elif("what day" in prompt and "today" in prompt):
+        return current_time.get_day("en")
+    elif("che" in prompt and "giorno" in prompt):
+        return current_time.get_day("it")
+    elif("che" in prompt and "ore sono" in prompt):
+        return current_time.get_time("it")
+    elif("che" in prompt and "ora è" in prompt):
+        return current_time.get_time()
 
+    else:
+        return ""
+
+async def handle_audio(websocket,trascript_queue,brain_queue, whisper_model):
     try:
-        porcupine = pvporcupine.create(
-            access_key=ACCESS_KEY,
-            library_path=args.library_path,
-            model_path=args.model_path,
-            keyword_paths=args.keyword_paths,
-            sensitivities=args.sensitivities)
-    except pvporcupine.PorcupineInvalidArgumentError as e:
-        print("One or more arguments provided to Porcupine is invalid: ", args)
-        print(e)
-        raise e
-    except pvporcupine.PorcupineActivationError as e:
-        print("AccessKey activation error")
-        raise e
-    except pvporcupine.PorcupineActivationLimitError as e:
-        print("AccessKey '%s' has reached it's temporary device limit" % args.access_key)
-        raise e
-    except pvporcupine.PorcupineActivationRefusedError as e:
-        print("AccessKey '%s' refused" % args.access_key)
-        raise e
-    except pvporcupine.PorcupineActivationThrottledError as e:
-        print("AccessKey '%s' has been throttled" % args.access_key)
-        raise e
-    except pvporcupine.PorcupineError as e:
-        print("Failed to initialize Porcupine")
-        raise e
+        encoded_audio = await websocket.recv()
+        with open("karpus.wav","wb") as f:
+            f.write(encoded_audio)
+            print("saved")
+            f.close()
+        
+        result_dict= transcribe.transcribe_audio("karpus.wav",whisper_model)
+        result=result_dict["text"]
 
-    keywords = list()
-    for x in keyword_paths:
-        keyword_phrase_part = os.path.basename(x).replace('.ppn', '').split('_')
-        if len(keyword_phrase_part) > 6:
-            keywords.append(' '.join(keyword_phrase_part[0:-6]))
+        result=remove_quotes(result,False) # remove from transcription quotes and duble quotes
+       
+        response=check_prompt(result) # check if time is asked
+
+        if(response==""): # se la risposta e' nulla significa che non e' stata fatta una richiesta specifica e la domanda va passata a llama
+            print("questo e' il risultato "+result)
+            cache_json=np.empty(1000000,dtype=dict)
+            counter=0
+            try:
+                with open("cache.text", "r") as file:
+                    for line in file:
+                        cache_json[counter]=(json.loads(line))
+                        counter=counter+1
+                    file.close()
+            except:
+                print("cache vuota")
+            for i in range(len(cache_json)):
+                if(i<counter):
+                    if(cache_json[i]["input"]==result):
+                            print("input in cache ")
+                            response=cache_json[i]["output"]
+                            language=cache_json[i]["lan"]
+                            await websocket.send(json.dumps({"text":response, "lan":language}))
+                            return
+            print("input nuovo elaboro con modello")
+
+            trascript_queue.put(result)
+            response=brain_queue.get()
+            response=remove_quotes(response,True)
+            with open("cache.text", "a") as file:
+                file.write(json.dumps({"input":result,"output":response,"lan":result_dict["language"]}) + '\n')
+                file.close()
+            await websocket.send(json.dumps({"text":response, "lan":result_dict["language"]}))
         else:
-            keywords.append(keyword_phrase_part[0])
+            print("processato")
+            await websocket.send(json.dumps({"text":response, "lan":result_dict["language"]}))
 
-    print('Porcupine version: %s' % porcupine.version)
 
-    recorder = PvRecorder(
-        frame_length=porcupine.frame_length,
-        device_index=args.audio_device_index)
-    recorder.start()
+    except websockets.exceptions.ConnectionClosedError:
+        print("Connection closed unexpectedly")
 
-    wav_file = None
-    if args.output_path is not None:
-        wav_file = wave.open(args.output_path, "w")
-        wav_file.setnchannels(1)
-        wav_file.setsampwidth(2)
-        wav_file.setframerate(16000)
-        
 
-    print('Listening ... (press Ctrl+C to exit)')    
-    try:
-        while True:
-            pcm = recorder.read()
-            result = porcupine.process(pcm)
+def main_server_transcribe(trascript_queue,brain_queue):
+    whisper_model=transcribe.setup_whisper()
+    server_address = "0.0.0.0"
+    server_port = 8700
 
-            if result >= 0:
-                #print('[%s] Detected %s' % (str(datetime.now()), keywords[result]))
-                try:
-                    stop_command()
-                    print("stoppeddddd")
-                except Exception as e:
-                    print(e)
-                    
-                print("im listening please go on")
-                arduino.write(bytes("listening"+'\n','utf-8'))
-                phrase = phrases[random.randint(0,len(phrases)-1)]
-                command=f'echo "{phrase}" |   ./piper/piper --model piper/en_US-kathleen-low.onnx --config piper/en_en_US_kathleen_low_en_US-kathleen-low.onnx.json --output-raw |   aplay -r 16000 -f S16_LE -t raw -'
-                os.system(command)
-                with sr.Microphone() as source:
-                    print("Say something!")
-                    audio = r.listen(source,)
-                arduino.write(bytes("led_stop"+'\n','utf-8'))
-                try:
-                    arduino.write(bytes("thinking"+'\n','utf-8'))
-                    result=send_wav_file_and_get_response(websocket_url=websocket_url,data=audio.get_wav_data())
-                    result=json.loads(result)
-                    print(result)
-                    if(result["lan"]=="it"):
-                        command=f'echo "{result["text"]}" |   ./piper/piper --model piper/it_IT-riccardo-x_low.onnx --config piper/it_it_IT_riccardo_x_low_it_IT-riccardo-x_low.onnx.json --output-raw |   aplay -r 16000 -f S16_LE -t raw -'
-                        start_command(command)
-                    else:
-                        command=f'echo "{result["text"]}" |   ./piper/piper --model piper/en_US-kathleen-low.onnx --config piper/en_en_US_kathleen_low_en_US-kathleen-low.onnx.json --output-raw |   aplay -r 16000 -f S16_LE -t raw -'
-                        start_command(command)
-                    arduino.write(bytes("led_stop"+'\n','utf-8'))
+    handle_audio_with_params = partial(handle_audio, trascript_queue=trascript_queue,brain_queue=brain_queue, whisper_model=whisper_model)
 
-                except:
-                    arduino.write(bytes("error"+'\n','utf-8'))
+    start_server = websockets.serve(handle_audio_with_params, server_address, server_port,)
 
-    except KeyboardInterrupt:
-        print('Stopping ...')
-    finally:
-        recorder.delete()
-        porcupine.delete()
-        
+    print(f"WebSocket server started at ws://{server_address}:{server_port}")
+
+    asyncio.get_event_loop().run_until_complete(start_server)
+    asyncio.get_event_loop().run_forever()
+
+def gpu_process(trascript_queue,brain_queue):
+    generator=set_up_generator()
+    while True:
+        message = trascript_queue.get()
+        print('sto processando '+message)
+        result=get_response(message,generator)
+        brain_queue.put(result)
 
 
 if __name__ == '__main__':
-    main()
+    # Create a multiprocessing Queue
+    trascript_queue = multiprocessing.Queue()
+    brain_queue=  multiprocessing.Queue()
+    # Create processes
+    gpu_proc = multiprocessing.Process(target=gpu_process, args=(trascript_queue,brain_queue))
+    cpu_proc = multiprocessing.Process(target=main_server_transcribe, args=(trascript_queue,brain_queue))
+    
+    # Start processes
+    gpu_proc.start()
+    time.sleep(120)
+    cpu_proc.start()
+
+    # Wait for both processes to finish
+    # gpu_proc.join()
+    # cpu_proc.join()
+    
