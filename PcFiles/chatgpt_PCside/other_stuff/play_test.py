0fa3bb2eca41f7fa833fc50d9b0ce1acc5af3ebe
michelottialessandro <alessandro.michelotti1706@gmail.com>
2024-09-12 21:25:37 +0200

    updated

--- PcFiles/chatgpt_PCside/other_stuff/play_test.py
+++
@@ -1,9 +0,0 @@
-
-# import required modules
-from pydub import AudioSegment
-from pydub.playback import play
- 
-# for playing wav file
-song = AudioSegment.from_wav("speech.wav")
-print('playing sound using  pydub')
-play(song)
--- PcFiles/train_zero_shot_classification/altro_modello.py
+++
@@ -1,8 +0,0 @@
-from transformers import pipeline
-classifier = pipeline("zero-shot-classification", model="MoritzLaurer/deberta-v3-large-zeroshot-v2.0")
-candidate_labels = ['asking for time','asking for date',"asking for weather","calculation"]
-while True:
-    input_=input("enter: ")
-    sequence_to_classify = input_
-    output = classifier(sequence_to_classify, candidate_labels, multi_label=False)
-    print(output)
