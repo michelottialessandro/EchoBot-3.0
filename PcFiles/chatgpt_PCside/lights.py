02ab5c8e60679b139f4eaa6cd4119ef055d67821
michelottialessandro <alessandro.michelotti1706@gmail.com>
2024-10-09 22:41:52 +0200

    cotrolla luci esp8266

---
+++ PcFiles/chatgpt_PCside/lights.py
@@ -0,0 +1,30 @@
+import websocket
+import json
+websocket_url = "ws://192.168.178.43/ws"
+ws_esp=websocket.create_connection(websocket_url)
+
+
+def lights_manager(is_on,text):
+    if(is_on):
+        if("blu" in text or "blue" in text):
+            json_data=json.dumps({"command":"BLUE","value":[0,0,255]})
+            ws_esp.send(json_data)
+            print("turing on blue")
+        elif("red"in text or "ross" in text): #potrebbe essere sia rosse sia rosso 
+            json_data=json.dumps({"command":"RED","value":[0,0,255]})
+            ws_esp.send(json_data)
+            print("turning on red")
+        elif("green" in text or "verd" in text): 
+            json_data=json.dumps({"command":"GREEN","value":[0,0,255]})
+            ws_esp.send(json_data)
+            print("turining on green")
+        else:
+            json_data=json.dumps({"command":"BLUE","value":[0,0,255]})
+            ws_esp.send(json_data)
+            print("turning on blue")
+    else:
+        json_data=json.dumps({"command":"OFF","value":[0,0,0]})
+        ws_esp.send(json_data)
+        print("turning off")
+
+
